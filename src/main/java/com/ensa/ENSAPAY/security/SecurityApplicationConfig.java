4933771c92039254d8920a385b0bce9e044623a1
Hamza-octav <zaimes1997@gmail.com>
2022-06-27 15:04:48 +0100

    back up

--- src/main/java/com/ensa/ENSAPAY/security/SecurityApplicationConfig.java
+++ src/main/java/com/ensa/ENSAPAY/security/SecurityApplicationConfig.java
@@ -58,7 +58,59 @@ public class SecurityApplicationConfig extends WebSecurityConfigurerAdapter
     public void adminCredentials()
     {
 
+        Creditor maroctelecom = new Creditor();
+        maroctelecom.setTitle("MAROC TELECOM");
+        maroctelecom.setDescription("Maroc Telecom, votre opérateur global de téléphonie fixe, mobile et internet.");
+        maroctelecom.setType(CreditorType.Creditor);
+        maroctelecom.setLogo("https://i.ibb.co/SsRPP8t/maroc-telecom-bleu-fr-grande.jpg");
+        creditorService.addCreditor(maroctelecom);
 
+        Creditor inwi = new Creditor();
+        inwi.setTitle("INWI");
+        inwi.setDescription("Découvrez les offres et services inwi, opérateur téléphonique mobile, fixe et internet au Maroc");
+        inwi.setType(CreditorType.Creditor);
+        inwi.setLogo("https://i.ibb.co/r0kLRbR/logo-seo.jpg");
+        creditorService.addCreditor(inwi);
+
+        Creditor orange = new Creditor();
+        orange.setTitle("ORANGE");
+        orange.setDescription("Bienvenue à tous chez Orange. Orange propose des offres et services de téléphonie mobile, d'internet et de fibre.");
+        orange.setType(CreditorType.Creditor);
+        orange.setLogo("https://i.ibb.co/mBkGK2n/Accueil.png");
+        creditorService.addCreditor(orange);
+
+        Creditor radeema = new Creditor();
+        radeema.setTitle("RADEEMA");
+        radeema.setDescription("RADEEMA instaure des mesures préventives pour assurer la continuité des services.");
+        radeema.setType(CreditorType.Creditor);
+        radeema.setLogo("https://i.ibb.co/r6hncRn/NL101-RADEEMA.jpg");
+        creditorService.addCreditor(radeema);
+
+        Creditor redal = new Creditor();
+        redal.setTitle("REDAL");
+        redal.setDescription("Le service e-facture est un service de Redal qui permet aux clients de recevoir leurs factures sur leurs adresses mail.");
+        redal.setType(CreditorType.Creditor);
+        redal.setLogo("https://i.ibb.co/mGv240N/Redal-2.jpg");
+        creditorService.addCreditor(redal);
+
+        Creditor alcs = new Creditor();
+        alcs.setTitle("ALCS");
+        alcs.setDescription("L'ALCS est la première et la plus importante association de lutte contre le sida constituée dans la région du Maghreb et du Moyen Orient.");
+        alcs.setType(CreditorType.Charity);
+        alcs.setLogo("https://i.ibb.co/QNDF5QY/images.jpg");
+        creditorService.addCreditor(alcs);
+
+        Admin admin   = new Admin();
+        admin.setUsername("admin");
+        admin.setPassword(passwordEncoder.encode("admin"));
+        admin.setRole("ADMIN");
+        adminService.addAdmin(admin);
+
+        Admin admin2   = new Admin();
+        admin2.setUsername("nizar");
+        admin2.setPassword(passwordEncoder.encode("motdepasse"));
+        admin2.setRole("ADMIN");
+        adminService.addAdmin(admin2);
      /*   Creditor maroctelecom = new Creditor();
         maroctelecom.setTitle("MAROC TELECOM");
         maroctelecom.setDescription("Maroc Telecom, votre opérateur global de téléphonie fixe, mobile et internet.");
